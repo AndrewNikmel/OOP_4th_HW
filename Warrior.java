fc1346e0989321f3308a6602df309482021dd359
unknown <Andrew M>
2023-04-29 17:20:37 +0300

    first steps are done

--- Warrior.java
+++ Warrior.java
@@ -1,3 +1,23 @@
-public class Warrior {
-    
+import java.util.Random;
+
+public abstract class Warrior {
+    private String name;
+    private Weapon weapon;
+    private int healthpoint;
+
+    public Warrior(String name, Weapon weapon, int healthpoint){
+        this.name = name;
+        this.weapon = weapon;
+        this.healthpoint = healthpoint;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name is %s, the weapon is %s, has %d healthpoints", name, weapon, healthpoint);
+    }
+
+    public int hit(){
+        Random random = new Random();
+        return random.nextInt(weapon.damage());
+    }
 }
